d5d995e9a215822571d4dce7b82124a487a7828b
Karun Japhet <karun@japhet.in>
2023-02-10 13:20:43 +0530

    Use kotlin constructs to find if expense is over type's limit

--- expensereport-kotlin/src/main/kotlin/com/nelkinda/training/ExpenseReport.kt
+++ expensereport-kotlin/src/main/kotlin/com/nelkinda/training/ExpenseReport.kt
@@ -2,13 +2,6 @@ package com.nelkinda.training
 
 import java.util.Date
 
-enum class ExpenseType(val reportName: String, private val highExpenseLimit: Int? = null) {
-    DINNER("Dinner", 5000), BREAKFAST("Breakfast", 1000), CAR_RENTAL("Car Rental");
-
-    fun isOverLimit(expenseAmount: Int): Boolean =
-        if (highExpenseLimit == null) false else expenseAmount > highExpenseLimit
-}
-
 data class Expense(val type: ExpenseType, val amount: Int = 0) {
     fun overLimitMarker(): String = if (type.isOverLimit(amount)) "X" else " "
 }
---
+++ expensereport-kotlin/src/main/kotlin/com/nelkinda/training/ExpenseType.kt
@@ -0,0 +1,9 @@
+package com.nelkinda.training
+
+enum class ExpenseType(val reportName: String, private val highExpenseLimit: Int? = null) {
+    DINNER("Dinner", 5000),
+    BREAKFAST("Breakfast", 1000),
+    CAR_RENTAL("Car Rental");
+
+    fun isOverLimit(expenseAmount: Int): Boolean = highExpenseLimit?.let { expenseAmount > highExpenseLimit } ?: false
+}
